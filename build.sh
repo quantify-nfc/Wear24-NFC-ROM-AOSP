aa4def594a253e4a4ebfbf7449b504a8e5aa07f3
davwheat <blokmaster.help@gmail.com>
2019-05-21 21:28:13 +0100

    fix(buildscript): add workaround for flex issue & cleanup before build

--- build.sh
+++ build.sh
@@ -12,9 +12,16 @@ fi
 export ANDROID_JACK_VM_ARGS="-Xmx8G -Xms1G -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 export JACK_SERVER_VM_ARGUMENTS="-Xmx8G -Xms1G -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 
+# Building on Ubuntu 18.04 causes an issue with the flex prebuilt package
+# This fixes it for some strange reason that none of us question because it lets us build AOSP :)
+export LC_ALL=C
+
 # Restart the Jack server to use the new arguments
 ./prebuilts/sdk/tools/jack-admin kill-server
 ./prebuilts/sdk/tools/jack-admin start-server
+
+# Make clobber (because why not)
+m clobber
 
 # Select the dorado product
 lunch full_dorado-userdebug
