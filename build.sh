b9530db307f1fcf1a341d1f1a9889e67d6af1e31
Jared Tamana <seanythedream@gmail.com>
2019-05-21 14:58:16 -0400

    fix(buildscipt): Install jack server if not present

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 # Setup build environment
 . build/envsetup.sh
 
+# install jack server if it isn't present
+if [ ! -d "~/.jack-server" ]; then
+	cd prebuilts/sdk/tools
+	./jack-admin install-server jack-launcher.jar jack-server-4.8.ALPHA.jar
+	cd ../../../
+fi
+
 # Set Java max heap size to 8GB (or AOSP won't build/fail at ~40-80%)
 export ANDROID_JACK_VM_ARGS="-Xmx8G -Xms1G -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 export JACK_SERVER_VM_ARGUMENTS="-Xmx8G -Xms1G -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
