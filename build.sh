c989fa888797c05c105ec5d8e280c9b36e54cb35
davwheat <blokmaster.help@gmail.com>
2019-05-21 22:00:32 +0100

    chore(buildscript): add niceties to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,36 @@
+#!/bin/bash
+
+timeout () {
+    tput sc
+    time=$1; while [ $time -ge 0 ]; do
+        tput rc; tput el
+        printf "$2" $time
+        ((time--))
+        sleep 1
+    done
+    tput rc; tput ed;
+}
+
+clear
+echo "Quantify | The \"official\" Wear24 ROM Project"
+echo "WIP by JaredTamana and davwheat (XDA)"
+echo "Thanks to osm0sis (mkbootimg) & mkorenko (some dorado makefiles)"
+echo "Special thanks to lexri for helping us with everything else"
+echo "Thanks to bensdeals for your kind donation!"
+echo "And Quanta/AOSP, I suppose :')"
+echo "---------------------------------------------------"
+timeout 5 "AOSP build begins in %s seconds."
+
 # Setup build environment
+echo
+echo
+echo "Setting up build environment..."
 . build/envsetup.sh
 
 # install jack server if it isn't present
+echo
+echo
+echo "Installing Jack server..."
 if [ ! -d "~/.jack-server" ]; then
 	cd prebuilts/sdk/tools
 	./jack-admin install-server jack-launcher.jar jack-server-4.8.ALPHA.jar
@@ -9,22 +38,33 @@ if [ ! -d "~/.jack-server" ]; then
 fi
 
 # Set Java max heap size to 8GB (or AOSP won't build/fail at ~40-80%)
+echo
+echo
+echo "Setting up Jack variables..."
 export ANDROID_JACK_VM_ARGS="-Xmx8G -Xms1G -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 export JACK_SERVER_VM_ARGUMENTS="-Xmx8G -Xms1G -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 
-# Building on Ubuntu 18.04 causes an issue with the flex prebuilt package
-# This fixes it for some strange reason that none of us question because it lets us build AOSP :)
-export LC_ALL=C
-
 # Restart the Jack server to use the new arguments
+echo
+echo
+echo "Restarting Jack server"
 ./prebuilts/sdk/tools/jack-admin kill-server
 ./prebuilts/sdk/tools/jack-admin start-server
 
 # Make clobber (because why not)
+echo
+echo
+echo "Cleaning up directory..."
 m clobber
 
 # Select the dorado product
+echo
+echo
+echo "Preparing dorado build..."
 lunch full_dorado-userdebug
 
 # Build with (total cores * 8) concurrent jobs
+echo
+echo
+echo "Building AOSP with $((`nproc`*8)) concurrent jobs..."
 m -j$((`nproc`*8))
