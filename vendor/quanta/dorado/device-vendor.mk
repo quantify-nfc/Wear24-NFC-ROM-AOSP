e395dd251d7ab737fbf0c1aa0ab4d9b506aa8e23
Jared Tamana <seanythedream@gmail.com>
2019-05-22 17:49:05 -0400

    begin creating driver makefiles

--- vendor/quanta/dorado/device-vendor.mk
+++ vendor/quanta/dorado/device-vendor.mk
@@ -13,3 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+LOCAL_STEM := dorado/device-partial.mk
+
+$(call inherit-product-if-exists, vendor/quanta/$(LOCAL_STEM))
