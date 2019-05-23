9e8fafc89760db72ed39b62b86b2a05f607efa12
davwheat <blokmaster.help@gmail.com>
2019-05-23 23:33:08 +0100

    feat(swapfile): add swapfile creation script

---
+++ swap-file-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+timeout () {
+  if [ NoTimeouts == false ]; then
+    tput sc
+    time=$1; while [ $time -ge 0 ]; do
+      tput rc; tput el
+      printf "$2" $time
+      ((time--))
+      sleep 1
+    done
+    tput rc; tput ed;
+  else
+    echo "*** Timeout skipped! ***"
+  fi
+}
+
+echo "This script sets up a 16 GB swapfile for your Ubuntu install"
+echo "It is recommended that you have more than 16 GB RAM or RAM + swap"
+echo
+timeout 10 "Starting swapfile setup in %s seconds..."
+
+sudo swapoff -a
+sudo fallocate -l 16G /16gswapfile
+sudo chmod 600 /16gswapfile
+sudo mkswap /16gswapfile
+sudo swapon /16gswapfile
+
+# Check the swap file was added
+sudo swapon --show
+
+# Make it permanent
+echo '/16gswapfile none swap sw 0 0' | sudo tee -a /etc/fstab
