f568ec68bed620f3b66f5072784c360ca0319364
JaredTheWolf <seanythedream@gmail.com>
2019-07-11 13:27:59 -0400

    fix(repo setup): add ccache to apt install

--- repo-setup.sh
+++ repo-setup.sh
@@ -43,7 +43,7 @@ git config http.postBuffer 157286400
 echo 
 echo 
 timeout 4 "Installing AOSP requirements in %s seconds"
-sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+sudo apt install git-core gnupg flex bison gperf build-essential zip curl ccache zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
 
 echo 
 echo 
