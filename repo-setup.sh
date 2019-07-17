2997a8eefcab651f29c8e6b6a9798ba4b939086c
David Wheatley <blokmaster.help@gmail.com>
2019-07-17 16:28:06 +0100

    feat(travis speed): skip unneeded commands for Travis
    [ci skip]

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 NoTimeouts=false
+Travis=false
 
 while [ "${1:-}" != "" ]; do
   case "$1" in
     "-n" | "--no-timeouts")
       NoTimeouts=true
       ;;
+    "-t" | "--travis-build")
+      NoTimeouts=true
+      Travis=true
   esac
   shift
 done
@@ -36,12 +40,12 @@ timeout 3 "Repo setup begins in %s seconds."
 
 echo 
 echo
-if [ "$NoTimeouts" == false ]; then # don't ask for user input when not using timeouts
+if [[ $NoTimeouts == false ]]; then # don't ask for user input when not using timeouts
   # read input into input variable, with a text prompt message
   read -p "Install OpenJDK 8? (We ask this in case you have a conflicting version) [Y/n]" input
 fi
 echo
-if [[ $input == "N" || $input == "n" ]]; then
+if [[ $input == "N" || $input == "n" || $Travis == true ]]; then
   # don't install openjdk when user has asked not to... obviously...
   echo "--==[[ NOT INSTALLING OpenJDK ]]==--"
 else
@@ -51,16 +55,18 @@ else
   sudo apt -y -qq install openjdk-8-jdk
 fi
 
-echo
-echo
-timeout 4 "Setting up git config"
-git config core.bigFileThreshold 15m
-git config http.postBuffer 157286400
+if [[ $Travis == false ]]; then
+  echo
+  echo
+  timeout 4 "Setting up git config"
+  git config core.bigFileThreshold 15m
+  git config http.postBuffer 157286400
 
-echo 
-echo 
-timeout 4 "Installing AOSP requirements in %s seconds"
-sudo apt -y -qq install git-core gnupg flex bison gperf build-essential zip curl ccache zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip >> /dev/null
+  echo 
+  echo 
+  timeout 4 "Installing AOSP requirements in %s seconds"
+  sudo apt -y -qq install git-core gnupg flex bison gperf build-essential zip curl ccache zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip >> /dev/null
+ fi
 
 echo 
 echo 
@@ -80,7 +86,13 @@ echo
 timeout 4 "Synchronising local files with AOSP"
 repo sync -c --no-clone-bundle -q -j$((`nproc`*2))
 
-echo
-echo
-echo "That should be it!"
-echo "To build, run '. build.sh'"
+if [[ $Travis == false ]]; then
+  echo
+  echo
+  echo "That should be it!"
+  echo "To build, run '. build.sh'"
+else
+  echo
+  echo 
+  echo "Setup complete"
+fi
